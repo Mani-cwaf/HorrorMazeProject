bd98bdfa41661309e147bce643e0fd802dedd001
Mani_Dev <dhingramsp@Gmail.com>
2022-09-19 23:44:06 +0530

    first release

--- Project/Horror Maze/Assets/Scripts/Player.cs
+++ Project/Horror Maze/Assets/Scripts/Player.cs
@@ -6,46 +6,34 @@ using UnityEngine.Rendering.Universal;
 public class Player : MonoBehaviour
 {
     public GameObject cameraObject;
-    List<GameObject> wallPrefabs;
+    public GameObject wallPrefab;
     public Rigidbody2D rb;
     Vector2 movement;
-    private void Start()
+    float speed = 40;
+    void Update()
     {
-        wallPrefabs = GameObject.FindGameObjectsWithTag("Wall").ToList();
-    }
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            movement += Vector2.up / 30;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            movement += Vector2.left / 30;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movement += Vector2.down / 30;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            movement += Vector2.right / 30;
-        }
+        movement = new Vector2(Input.GetAxis("Horizontal") / speed, Input.GetAxis("Vertical") / speed);
         rb.MovePosition(rb.position + movement);
         movement = Vector2.zero;
-        cameraObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
+
+        cameraObject.transform.localPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Light Item"))
         {
             Destroy(collision.gameObject);
-            gameObject.GetComponentInChildren<Light2D>().pointLightOuterRadius += 2f / gameObject.GetComponentInChildren<Light2D>().pointLightOuterRadius;
+            gameObject.GetComponentInChildren<Light2D>().pointLightOuterRadius += 0.5f;
+        }
+        if (collision.gameObject.CompareTag("Speed Item"))
+        {
+            Destroy(collision.gameObject);
+            speed -= 2.5f;
         }
         if (collision.gameObject.CompareTag("See Through Walls Item"))
         {
             Destroy(collision.gameObject);
-            wallPrefabs.ForEach(a => a.GetComponent<ShadowCaster2D>().enabled = false);
+            wallPrefab.GetComponentsInChildren<ShadowCaster2D>().ToList().ForEach(a => a.enabled = false);
         }
     }
 }
